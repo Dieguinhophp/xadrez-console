3b1ef0dacdebc2ef84cc5b1571ed9c1bdaac0ac4
Dieguinhophp <lorddedsbrgamers2@gmail.com>
2026-04-23 19:20:04 -0300

    Primeiro Commit

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using tabuleiro;
+
+
+namespace xadrez_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.imprimirTabuleiro(tab);
+
+            Console.ReadLine();
+        }
+    }
+}
